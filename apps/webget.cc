b7942a70f730d8143353af08f775a050fd794d6b
郭申申 <3501641633@qq.com>
2025-10-20 09:45:46 +0800

    test

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,14 +9,6 @@ using namespace std;
 void get_URL(const string &host, const string &path) {
     // Your code here.
 
-    // You will need to connect to the "http" service on
-    // the computer whose name is in the "host" string,
-    // then request the URL path given in the "path" string.
-
-    // Then you'll need to print out everything the server sends back,
-    // (not just one call to read() -- everything) until you reach
-    // the "eof" (end of file).
-
     Address address(host, "http");
     TCPSocket tcp_socket;
     tcp_socket.connect(address);
